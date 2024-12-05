1368e1cd49301ccb46eaa7e531bb1259cbeab041
Bimash Maharjan <bmasmhj@gmail.com~>
2024-12-05 16:16:55 +0545

    kanban board component

---
+++ src/app/kanban/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-kanban-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'], 
+})
+
+export class KanbanBoardComponent {
+
+}
